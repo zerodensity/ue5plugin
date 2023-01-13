6171d5382e8bdf0cfceea9fdcf87ea15c2aff64c
Yunus Yurttürk <yunus.yurtturk@zerodensity.tv>
2023-01-13 11:46:23 +0300

    Base class for editor tickable actors added

---
+++ Source/MZClient/Private/MZEditorTickableActor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MZEditorTickableActor.h"
+
+// Sets default values
+AMZEditorTickableActor::AMZEditorTickableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMZEditorTickableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMZEditorTickableActor::Tick(float DeltaTime)
+{
+#if WITH_EDITOR
+	if (GetWorld() != nullptr && GetWorld()->WorldType == EWorldType::Editor)
+	{
+		BlueprintEditorTick(DeltaTime);
+	}
+	else
+#endif
+	{
+		Super::Tick(DeltaTime);
+	}
+}
+
+
+bool AMZEditorTickableActor::ShouldTickIfViewportsOnly() const
+{
+	if (GetWorld() != nullptr && GetWorld()->WorldType == EWorldType::Editor && UseEditorTick)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
