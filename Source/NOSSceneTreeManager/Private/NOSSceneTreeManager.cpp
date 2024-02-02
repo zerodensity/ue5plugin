84f2c3055eb73d1792b692e1c809fc0b9b04c2b4
dogukannn <dogukan.ozturk@mediaz.io>
2024-02-02 16:11:13 +0300

    disable delete actor for reality parent transform

--- Source/NOSSceneTreeManager/Private/NOSSceneTreeManager.cpp
+++ Source/NOSSceneTreeManager/Private/NOSSceneTreeManager.cpp
@@ -566,6 +566,15 @@ void FNOSSceneTreeManager::OnNOSContextMenuRequested(nos::ContextMenuRequest con
 			{
 				return;
 			}
+
+			if (actorNode->nosMetaData.Contains(NosMetadataKeys::spawnTag))
+			{
+				if (actorNode->nosMetaData.FindRef(NosMetadataKeys::spawnTag) == FString("RealityParentTransform"))
+				{
+					return;
+				}
+			}
+
 			flatbuffers::FlatBufferBuilder mb;
 			std::vector<flatbuffers::Offset<nos::ContextMenuItem>> actions = menuActions.SerializeActorMenuItems(mb);
 			auto posx = nos::fb::vec2(pos.X, pos.Y);
