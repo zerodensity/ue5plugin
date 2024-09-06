6367493f163168e3cf3ecd0d80ac4efec0ad7001
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-09-06 15:39:58 +0300

    From 5.4nos1.1: Unregister deleted actors' functions from Scene Tree
    Co-authored-by: Caner <caner.coskun@nodos.dev>

--- Source/NOSSceneTreeManager/Private/NOSActorFunctions.cpp
+++ Source/NOSSceneTreeManager/Private/NOSActorFunctions.cpp
@@ -1,6 +1,7 @@
 // Copyright MediaZ Teknoloji A.S. All Rights Reserved.
 
 #include "NOSActorFunctions.h"
+#include "NOSSceneTreeManager.h"
 
 
 NOSFunction::NOSFunction(UObject* container, UFunction* function)
@@ -35,6 +36,11 @@ flatbuffers::Offset<nos::fb::Node> NOSFunction::Serialize(flatbuffers::FlatBuffe
 
 void NOSFunction::Invoke() // runs in game thread
 {
+	if (!IsValid(Container))
+	{
+		UE_LOG(LogNOSSceneTreeManager, Error, TEXT("Unable to invoke function %s on invalid container"), *FunctionName);
+		return;
+	}
 	Container->Modify();
 	Container->ProcessEvent(Function, Parameters);
 }
